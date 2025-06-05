5f6bc9706df0bf935ea849cb92dcb0b8def9e4b1
reshm <reshmapsuresh20@gmail.com>
2025-06-05 19:01:07 +0530

    feat: Retrieval Aged Brie item from ItemUpdaterFactory

--- src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/factory/ItemUpdaterFactory.java
@@ -1,4 +1,23 @@
 package com.gildedrose.factory;
 
+import com.gildedrose.ItemType;
+import com.gildedrose.updater.AgedBrie;
+import com.gildedrose.updater.ItemUpdater;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class ItemUpdaterFactory {
+
+    private static final Map<String, Supplier<ItemUpdater>> ITEM_SUPPLIER = createItemsMap ();
+
+    private static Map<String, Supplier<ItemUpdater>> createItemsMap () {
+        Map<String, Supplier<ItemUpdater>> map = new HashMap<> ();
+        map.put (ItemType.AGED_BRIE.getItemName (), AgedBrie::new);
+        return map;
+    }
+
+    public static ItemUpdater getItems (String itemType) {
+        return ITEM_SUPPLIER.get (itemType).get ();
+    }
 }
---
+++ src/test/java/com/gildedrose/factory/ItemUpdaterFactoryTest.java
@@ -0,0 +1,19 @@
+package com.gildedrose.factory;
+
+import com.gildedrose.ItemType;
+import com.gildedrose.updater.AgedBrie;
+import com.gildedrose.updater.ItemUpdater;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ItemUpdaterFactoryTest {
+
+    @Test
+    @DisplayName("Retrieving Aged Brie item")
+    void getItemAgedBrie() {
+
+        ItemUpdater agedBrie = ItemUpdaterFactory.getItems (ItemType.AGED_BRIE.getItemName ());
+        assertTrue (agedBrie instanceof AgedBrie);
+    }
+}
